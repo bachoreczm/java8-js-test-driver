4bbdd9d6cc01d0907bc726ec512a7b1830f030de
Bachorecz Mátyás <bachoreczm@gmail.com>
2016-01-28 20:24:58 +0100

    travisUpdate

--- src/test/java/jstester/JsTesterTest.java
+++ src/test/java/jstester/JsTesterTest.java
@@ -4,6 +4,7 @@ import static jstester.JsTester.JS_TEST_UTIL;
 import static jstester.JsTester.runTestsAndGetErrors;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
@@ -46,13 +47,11 @@ public class JsTesterTest {
     String testFile = "jstester.test_for_stack_trace";
     String srcFile = "jstester.source";
     String stackTraces = getStackTrace(testFile, srcFile);
-    throw new IllegalStateException(stackTraces);
-    // assertTrue(stackTraces.contains("AssertationError"));
-    // assertTrue(stackTraces.contains("at assertEquals (test_util.js:44)"));
-    // assertTrue(stackTraces,
-    // stackTraces.contains("at bug (jstester.source.js:17)"));
-    // assertTrue(
-    // stackTraces.contains("at test4 (jstester.test_for_stack_trace.js:20)"));
+    assertTrue(stackTraces.contains("AssertationError"));
+    assertTrue(stackTraces.contains("at assertEquals (test_util.js:44)"));
+    assertTrue(stackTraces.contains("at bug (jstester.source.js:17)"));
+    assertTrue(
+        stackTraces.contains("at test4 (jstester.test_for_stack_trace.js:20)"));
   }
 
   @Test
@@ -76,13 +75,11 @@ public class JsTesterTest {
     String srcFile1 = "jstester.source";
     String srcFile2 = "jstester.caller";
     String stackTraces = getStackTrace(testFile, srcFile1, srcFile2);
-    throw new IllegalStateException(stackTraces);
-    // assertTrue(stackTraces,
-    // stackTraces.contains("TypeError: Cannot call undefined"));
-    // assertTrue(stackTraces.contains("at bug (jstester.source.js:17)"));
-    // assertTrue(stackTraces.contains("at callBug (jstester.caller.js:6)"));
-    // assertTrue(stackTraces
-    // .contains("at testCallBug (jstester.test_for_multi_source.js:5)"));
+    assertTrue(stackTraces.contains("TypeError: Cannot call undefined"));
+    assertTrue(stackTraces.contains("at bug (jstester.source.js:17)"));
+    assertTrue(stackTraces.contains("at callBug (jstester.caller.js:6)"));
+    assertTrue(stackTraces
+        .contains("at testCallBug (jstester.test_for_multi_source.js:5)"));
   }
 
   private String getStackTrace(String... codes) throws IOException {
