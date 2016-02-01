73138b7a36e89a5517c2a5f82918be04a80b7b7c
Bachorecz Mátyás <bachoreczm@gmail.com>
2016-02-01 20:48:20 +0100

    plugin aggregator tests

--- src/test/java/jstester/plugins/JsTestPluginAggregatorTest.java
+++ src/test/java/jstester/plugins/JsTestPluginAggregatorTest.java
@@ -2,49 +2,53 @@ package jstester.plugins;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Iterator;
 
 import javax.script.ScriptException;
 
+import org.junit.Test;
+
 import jstester.JsFile;
 import jstester.plugins.defaultplugin.DefaultJsTestPlugin;
-
-import org.junit.Test;
 
 public class JsTestPluginAggregatorTest {
 
   @Test
-  public void testInit() {
+  public void theDefaultPluginIsNeverNull() {
     JsTestPluginAggregator aggregator = new JsTestPluginAggregator();
+
     assertEquals(DefaultJsTestPlugin.class, aggregator.getDefault().getClass());
-    assertNotNull(aggregator.getDefault());
-    assertNotNull(JsTestPluginAggregator.empty());
   }
 
   @Test
-  public void testAddAndIterate() throws IOException, ScriptException {
-    JsTestPlugin plugin1 = new PluginMock();
-    JsTestPlugin plugin2 = new PluginMock();
+  public void theEmptyAggregatorContainsDefaultPluginAndNoOther() {
+    JsTestPluginAggregator empty = JsTestPluginAggregator.empty();
+
+    containsDefaultAndNoOther(empty);
+  }
+
+  private void containsDefaultAndNoOther(JsTestPluginAggregator empty) {
+    assertEquals(DefaultJsTestPlugin.class, empty.getDefault().getClass());
+    Iterator<JsTestPlugin> iterator = empty.iterator();
+    iterator.next();
+    assertFalse(iterator.hasNext());
+  }
+
+  @Test
+  public void addFunctionAddsPlugins() throws IOException, ScriptException {
+    PluginMock plugin1 = new PluginMock();
+    PluginMock plugin2 = new PluginMock();
     JsTestPluginAggregator aggregator = new JsTestPluginAggregator();
     aggregator.add(plugin1);
     aggregator.add(plugin2);
+
     for (JsTestPlugin plugin : aggregator) {
       plugin.eval(new JsFile[0]);
     }
-    assertEquals(1, ((PluginMock) plugin1).getCalled());
-    assertEquals(1, ((PluginMock) plugin2).getCalled());
-  }
 
-  @Test
-  public void testHasNextPlugin() {
-    JsTestPluginAggregator empty = JsTestPluginAggregator.empty();
-    Iterator<JsTestPlugin> iterator = empty.iterator();
-    assertTrue(iterator.hasNext());
-    assertEquals(DefaultJsTestPlugin.class, iterator.next().getClass());
-    assertFalse(iterator.hasNext());
+    assertEquals(1, plugin1.getCalled());
+    assertEquals(1, plugin2.getCalled());
   }
 }
